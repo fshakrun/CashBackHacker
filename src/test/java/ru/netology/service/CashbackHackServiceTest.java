331a4332892e1e30dc6ba31e03d36019e7373495
Fedor Shakrun <fshakrun@gmail.com>
2022-12-12 14:36:43 +0300

    tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateAmount() {
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateAmountLess100() {
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateAmountLess1() {
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateTheAmountIf1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+}
